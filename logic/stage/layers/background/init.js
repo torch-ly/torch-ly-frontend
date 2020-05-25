871044537815a52c286e08fdccab612fff40cb92
Caesar0815 <gingermk007@gmail.com>
2020-05-25 12:26:22 +0200

    added rotationsnaps

--- logic/stage/layers/background/init.js
+++ logic/stage/layers/background/init.js
@@ -26,24 +26,29 @@ export function init() {
     tr: null
   });
 
-  let tr = new Konva.Transformer();
-  tr.nodes([rect1]);
-  tr.visible(false);
-  rect1.tr = tr;
-
-  let tr2 = new Konva.Transformer();
-  tr2.nodes([rect2]);
-  rect2.tr = tr2;
-
   out.push(rect1, rect2);
 
+  for (let object of out) {
+    let tr = new Konva.Transformer({
+      nodes: [object],
+      visible: false,
+      rotationSnaps: [0, 90, 180, 270]
+    });
+    object.tr = tr;
+  }
+
+  //TODO snap to grid: https://codepen.io/pierrebleroux/pen/gGpvxJ?editors=1010
+
   stage.on('click', function (e) {
+    //TODO solve double click problem
+    // tr.forceUpdate() ??
     function disableAll() {
       for (let object of out) {
         object.tr.visible(false);
         object.draggable(false);
       }
     }
+
     if (e.target == stage) {
       disableAll();
     } else {
