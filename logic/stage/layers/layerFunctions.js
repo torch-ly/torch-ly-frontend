3c5f4b16374e3577241f78543d2e9b380429afef
EricHier <developer.erichier@gmail.com>
2020-05-29 18:39:10 +0200

    fixed drag'n'drop in layerFunctions.js for painting mode and did some refactoring fixed #1 and #2

--- logic/stage/layers/layerFunctions.js
+++ logic/stage/layers/layerFunctions.js
@@ -1,6 +1,7 @@
 import {blockSnapSize} from "./grid/main";
 import {stage} from "../main";
 import Konva from "konva";
+import {store} from "../main";
 
 export function addSnapToGridListener(objects) {
   for (let object of objects) {
@@ -11,6 +12,7 @@ export function addSnapToGridListener(objects) {
     });
   }
 }
+
 //TODO Array To VarArgs
 export function snapToGrid(object) {
   let x = object.hasOwnProperty("attrs") ? object.attrs.x : object.x();
@@ -34,7 +36,7 @@ export function addTransformer(toAdd) {
   }
 }
 
-export function disableAll(objects) {
+function disableAll(objects) {
   for (let object of objects) {
     try {
       if (object.hasOwnProperty("tr")) {
@@ -46,65 +48,50 @@ export function disableAll(objects) {
   }
 }
 
-export function removeTransformer(objects) {
-  for (let object of objects) {
-    object.draggable = false;
-    object.attrs.draggable = false;
-    console.log(object)
-    object.stopDrag();
-  }
-}
-
 export function addTransformerClickListener(toListen) {
   stage.on('click', function (e) {
-    if (e.target == stage) {
-      disableAll(toListen);
-    } else {
-      for (let object of toListen) {
-        if (e.target == object) {
-          disableAll(toListen);
-          object.tr.visible(true);
-          try {
-            object.draggable(true);
-          } catch (e) {
 
-          }
-          object.moveToTop();
-          object.tr.moveToTop();
-        }
-      }
+    // return if not im move mode as no dragging should be disabled
+    if (!store.state.manu.move)
+      return;
+
+    disableAll(toListen);
+
+    for (let object of toListen) {
+
+      if (e.target !== object)
+        continue;
+
+      object.tr.visible(true);
+      try {
+        object.draggable(true);
+      } catch (e) { }
+
+      object.moveToTop();
+      object.tr.moveToTop();
     }
+
     stage.batchDraw();
   });
 }
 
 export function setStageDragAndDrop(enable) {
-  enable ? stage.draggable(true) : stage.draggable(false);
+  stage.draggable(enable);
 
   for (let layer of stage.children) {
     setLayerDragAndDrop(layer, enable);
   }
 }
 
+
 export function setLayerDragAndDrop(layer, enable) {
-  if (layer != stage.children[1]) {
-    return;
-  }
   for (let object of layer.children) {
-    if (enable) {
-      try {
-        object.draggable(true);
-      } catch (e) {
-      }
-    } else {
-      if (object.tr != null) {
-        object.tr.visible(false);
-      }
-      try {
-        object.draggable(false);
-      } catch (e) {
-      }
-    }
+    try {
+      object.draggable(enable);
+      if (enable)
+        object.tr.visible(true);
+    } catch (e) { }
   }
+
   stage.batchDraw();
 }
