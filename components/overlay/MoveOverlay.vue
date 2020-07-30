fb0d6037e44efd2495ba5860979cadb2e4bc5d93
Camaendir <bentemme.katec@gmail.com>
2020-07-30 12:32:51 +0200

    Added Image Adding Popup

--- components/overlay/MoveOverlay.vue
+++ components/overlay/MoveOverlay.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="m-6">
     <select required v-bind:value="$store.state.manu.layer" @input="dropdownChange"
-            class="w-full text-black p-1 rounded-full mb-4">
+            class="w-full text-black p-1 rounded-full mb-4" ref="layerdropdown" @load="dropdownChange">
       <option>Background</option>
       <option>Token</option>
     </select>
 
     <div class="hr"/>
 
-    <button class="submit-button active:submit-button-active mb-4 mt-4" @click="openPopup">Add Character</button>
+    <button ref="add_character_button" class="submit-button active:submit-button-active mb-4 mt-4" @click="openTokenPopup">Add Character</button>
 
-    <div class="hr"/>
-
-    <form id="backgroud-pos" v-on:submit.prevent="updateBackgroundObject" class="mt-2">
+    <form ref="background_pos_form" id="backgroud-pos" v-on:submit.prevent="updateBackgroundObject" class="mt-2" style="display: none">
       X: <input ref="x" name="x" type="number" v-bind:value="$store.state.selectedBackgroundObject.x"
                 class="input-field">
       <br>
@@ -28,9 +26,21 @@
       <input type="submit" class="submit-button active:submit-button-active mt-4">
     </form>
 
-    <div class="hr my-4"/>
+    <div ref="hr_background_divider" class="hr mt-4" style="display: none"/>
 
-    <FileUpload class="font-lg" />
+    <button ref="add_background_button" style="display: none" class="submit-button active:submit-button-active mb-4 mt-4" @click="openBackgroundPopup">Add Shape/Image</button>
+
+    <!--<div class="hr my-4"/>
+
+    <div id="background_image_adder" ref="background_image_adder">
+      <FileUpload ref="imageupload" class="font-lg" />
+      <br>
+      <div class="flex flex-col justify-center items-center"> <div class="text-center mb2">URL:</div> <input type="url" class="input-field"></div>
+      <div class="flex flex-col">
+        <button class="submit-button mb-3" @click="useImageToBackground">Use Uploaded URL</button> <button class="submit-button" @click="useURLToBackground">Use Given URL</button>
+      </div>
+    </div>-->
+
 
   </div>
 </template>
@@ -49,7 +59,27 @@
     },
     components: {LoadingSpinner, FileUpload},
     methods: {
+      useImageToBackground(e){
+
+      },
+
+      useURLToBackground(e){
+
+      },
+
       dropdownChange(e) {
+        console.log(e.target.value);
+        if(e.target.value !== "Background"){
+          this.$refs.add_character_button.style.display = "";
+          this.$refs.background_pos_form.style.display = "none";
+          this.$refs.hr_background_divider.style.display = "none";
+          this.$refs.add_background_button.style.display = "none";
+        }else{
+          this.$refs.add_character_button.style.display = "none";
+          this.$refs.background_pos_form.style.display = "";
+          this.$refs.hr_background_divider.style.display = "";
+          this.$refs.add_background_button.style.display = "";
+        }
         clearTransformerNodes();
         stage.batchDraw();
         this.$store.commit("manu/setLayer", e.target.value);
@@ -63,8 +93,12 @@
           snapToGrid: this.$refs.snapToGrid.checked
         });
       },
-      openPopup() {
-        this.$root.$emit("openCharacterPopup")
+      openTokenPopup() {
+        this.$root.$emit("openCharacterPopup");
+      },
+
+      openBackgroundPopup(){
+        this.$root.$emit('openImagePopup');
       }
     },
     mounted() {
