ca4807767258e04346c35eaf4303cb236315773f
EricHier <developer.erichier@gmail.com>
2020-09-01 16:02:54 +0200

    show network error only when it really occours

--- components/FloatingInfos.vue
+++ components/FloatingInfos.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hidden md:block fixed bottom-0 left-0">
     <div class="relative m-6 select-none">
-      <div class="w-auto flex flex-col button mb-4 border-4 border-red-400" @click="reload">
+      <div class="w-auto flex flex-col button mb-4 border-4 border-red-400" @click="reload" v-show="$store.state.errors.errors.size">
         <fa icon="bolt" class="block mx-auto text-red-400 text-3xl"/>
         <span class="block w-full text-center mt-1 font-500">Network Error</span>
       </div>
