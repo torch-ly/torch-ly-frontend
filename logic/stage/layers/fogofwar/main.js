a8e6beaceb650b1e98fc64dc9c2862836f9c44d4
Camaendir <bentemme.katec@gmail.com>
2020-09-01 19:24:02 +0200

    added containing slicing

--- logic/stage/layers/fogofwar/main.js
+++ logic/stage/layers/fogofwar/main.js
@@ -10,66 +10,29 @@ let currentPolygon = [];
 let nextPolygon;
 let insert = true;
 
+//init
 export function draw(player){
   layer = player;
   addFogOfWarListener();
 }
 
-export function DeletePolygon() {
-  try {
-    let coordinates = [[...currentPolygon, currentPolygon[0]]];
-    let turf_polygon = turf.polygon(coordinates);
-    currentPolygon = []
-    if (nextPolygon != null) {
-      nextPolygon.destroy();
-    }
-    let inside = [];
-    let intersect = [];
+function addFogOfWarListener() {
+  stage.on('mouseup', () => {
+    if(!store.state.manu.fogOfWar)
+      return;
+    let point_x_y = getRelativePointerPosition(stage);
+    addPointToPolygon([point_x_y.x, point_x_y.y])
+  }),
 
-    for (let i = 0; i < turf_polygons.length; i++) {
-      if (turf.booleanContains(turf_polygon, turf_polygons[i])) {
-        inside.push(i);
-        continue;
-      }
-      if (turf.booleanOverlap(turf_polygon, turf_polygons[i])) {
-        intersect.push(i);
-      }
-    }
-
-    for (let i of intersect) {
-      let difference = turf.difference(turf_polygons[i], turf_polygon);
-      let len = difference.geometry.coordinates.length;
-      polygons[i].destroy();
-
-      if (len > 1) {
-        for (let j = 0; j < len; j++) {
-          let split_poly = turf.polygon(difference.geometry.coordinates[j]);
-          if (j === 0) {
-            polygons[i] = addPolygons(split_poly, "#000");
-            turf_polygons[i] = split_poly;
-          } else {
-            polygons.push(addPolygons(split_poly, "#000"));
-            turf_polygons.push(split_poly);
-          }
-        }
-      } else {
-        polygons[i] = addPolygons(difference, "#000");
-        turf_polygons[i] = turf_polygons[i];
-      }
-    }
-
-  for(let i of inside.sort().reverse()){
-    turf_polygons.splice(i, 1);
-    polygons[i].destroy();
-    polygons.splice(i, 1);
-  }
-
-  } catch(e) {
-    alert("Bitte neu zeichen")
-  }
-  layer.batchDraw();
+    stage.on('mousemove', () => {
+      if(!store.state.manu.fogOfWar)
+        return;
+      let point_x_y = getRelativePointerPosition(stage);
+      showNextPoint([point_x_y.x, point_x_y.y])
+    });
 }
 
+//graphql
 export function syncronize(){
   let polygons = [];
   for(let i = 0; i < turf_polygons.length; i++){
@@ -96,6 +59,7 @@ export function recieveSyncronize(p_polygons) {
   layer.draw();
 }
 
+//frontend toggle
 export function toggleInsert() {
   currentPolygon = []
 
@@ -107,7 +71,10 @@ export function toggleInsert() {
   insert = !insert;
 }
 
+//insert and delete
 export function InsertPolygon(){
+  if(currentPolygon.length < 3)
+    return;
   let coordinates = [[...currentPolygon, currentPolygon[0]]];
   let turf_polygon = turf.polygon(coordinates);
   currentPolygon = []
@@ -148,6 +115,100 @@ export function InsertPolygon(){
   layer.batchDraw();
 }
 
+export function DeletePolygon() {
+  if(currentPolygon.length < 3)
+    return;
+  try {
+    let coordinates = [[...currentPolygon, currentPolygon[0]]];
+    let turf_polygon = turf.polygon(coordinates);
+    currentPolygon = []
+    if (nextPolygon != null) {
+      nextPolygon.destroy();
+    }
+    let inside = [];
+    let intersect = [];
+    let overlayed = [];
+
+    for (let i = 0; i < turf_polygons.length; i++) {
+      if (turf.booleanContains(turf_polygon, turf_polygons[i])) {
+        inside.push(i);
+        continue;
+      }
+
+      if(turf.booleanContains(turf_polygons[i], turf_polygon)){
+        overlayed.push(i)
+        continue;
+      }
+
+      if (turf.booleanOverlap(turf_polygon, turf_polygons[i])) {
+        intersect.push(i);
+      }
+    }
+
+    for (let i of overlayed){
+      let j = 0;
+      let line;
+      while(true){
+        line = turf.lineString([turf_polygon.geometry.coordinates[0][0], turf_polygons[i].geometry.coordinates[0][j]]);
+        if(turf.booleanContains(turf_polygons[i], line)){
+          break;
+        }
+        j++;
+      }
+
+      console.log("length: " + j + " - " + turf_polygons[i].geometry.coordinates[0].length);
+      let coordinates = turf_polygons[i].geometry.coordinates[0].splice(0, j+1)
+      for (let k = 0; k < turf_polygon.geometry.coordinates[0].length; k++){
+        coordinates.push(turf_polygon.geometry.coordinates[0][k]);
+      }
+      coordinates.push(coordinates[j]);
+      for (let point of turf_polygons[i].geometry.coordinates[0]){
+        coordinates.push(point);
+      }
+      console.log(coordinates);
+      let newpolygon = turf.polygon([coordinates]);
+
+      polygons[i].destroy();
+      polygons[i] = addPolygons(newpolygon, "#000");
+      turf_polygons[i] = newpolygon;
+    }
+
+    for (let i of intersect) {
+      let difference = turf.difference(turf_polygons[i], turf_polygon);
+      let len = difference.geometry.coordinates.length;
+      polygons[i].destroy();
+
+      if (len > 1) {
+        for (let j = 0; j < len; j++) {
+          let split_poly = turf.polygon(difference.geometry.coordinates[j]);
+          if (j === 0) {
+            polygons[i] = addPolygons(split_poly, "#000");
+            turf_polygons[i] = split_poly;
+          } else {
+            polygons.push(addPolygons(split_poly, "#000"));
+            turf_polygons.push(split_poly);
+          }
+        }
+      } else {
+        polygons[i] = addPolygons(difference, "#000");
+        turf_polygons[i] = turf_polygons[i];
+      }
+    }
+
+    for(let i of inside.sort().reverse()){
+      turf_polygons.splice(i, 1);
+      polygons[i].destroy();
+      polygons.splice(i, 1);
+    }
+
+  } catch(e) {
+    //alert("Bitte neu zeichen")
+    console.log(e);
+  }
+  layer.batchDraw();
+}
+
+//helper functions
 function addPolygons(turf, color){
   let konva_line = turf_to_Konva(turf);
 
@@ -156,6 +217,10 @@ function addPolygons(turf, color){
   layer.add(konva_line);
 
   return konva_line;
+}
+
+function addPointToPolygon(point){
+  currentPolygon.push(point);
 }
 
 function turf_to_Konva(turf_poylgon){
@@ -171,6 +236,7 @@ function turf_to_Konva(turf_poylgon){
   });
 }
 
+//draw temporary polygon
 function showNextPoint(point) {
   let points = JSON.parse(JSON.stringify(currentPolygon));
 
@@ -190,25 +256,4 @@ function showNextPoint(point) {
 
   layer.add(nextPolygon);
   layer.batchDraw();
-}
-
-function addPointToPolygon(point){
-  currentPolygon.push(point);
-}
-
-
-function addFogOfWarListener() {
-  stage.on('mouseup', () => {
-    if(!store.state.manu.fogOfWar)
-      return;
-    let point_x_y = getRelativePointerPosition(stage);
-    addPointToPolygon([point_x_y.x, point_x_y.y])
-  }),
-
-  stage.on('mousemove', () => {
-    if(!store.state.manu.fogOfWar)
-      return;
-    let point_x_y = getRelativePointerPosition(stage);
-    showNextPoint([point_x_y.x, point_x_y.y])
-  });
 }
