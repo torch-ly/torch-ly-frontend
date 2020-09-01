42ff1ef0cf248928df5a066ee6f4e52cb8075408
EricHier <developer.erichier@gmail.com>
2020-09-01 13:26:10 +0200

    minor hotkey improvements

--- logic/hotkey.js
+++ logic/hotkey.js
@@ -3,27 +3,28 @@ import {store} from "~/logic/stage/main";
 import {saveBackgroundLayer} from "~/logic/stage/layers/background/init";
 
 // Set Layer to background layer
-hotkeys('command+b,ctrl+b', function(event, handler){
+hotkeys('command+b,ctrl+b', () => {
   store.commit("manu/setHand");
   store.commit("manu/setLayer", "Background")
 });
 
 
 // Set Layer to token layer
-hotkeys('command+t,ctrl+t', function(event, handler){
+hotkeys('command+t,ctrl+t', () => {
   store.commit("manu/setHand");
   store.commit("manu/setLayer", "Token")
 });
 
 // Open Monster search
-hotkeys('command+m,ctrl+m', function(event, handler){
+hotkeys('command+m,ctrl+m', () => {
   store.commit("manu/setMonsters");
 });
 
 // Save Background Layer
-hotkeys('command+s,ctrl+s', function (event, handler){
+hotkeys('command+s,ctrl+s', (event) => {
   if (store.state.manu.layer !== "Background")
     return;
 
   saveBackgroundLayer();
-})
+  event.preventDefault()
+});
