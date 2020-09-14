3cd906853b7d576055cc12990327b383a0d975fa
Caesar0815 <gingermk007@gmail.com>
2020-09-14 15:00:02 +0200

    design improvements in fog of war gui

--- components/overlay/FogOfWarOverlay.vue
+++ components/overlay/FogOfWarOverlay.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
-    <button class="button" @click="inputClick">{{InsertButtonText}}</button>
-    <div class="hr"/>
-    <button class="button" @click="toggleClick">Toggle Delete/Insert</button>
-    <div class="hr"></div>
+
+    <div class="flex flex-row">
+      <label class="switch" @click="toggleClick()">
+        <input type="checkbox" v-model="insert">
+        <span class="slider round"></span>
+      </label>
+      <div class="flex items-center ml-2">Insert / Delete</div>
+    </div>
+
+    <button class="submit-button active:submit-button-active mt-2" @click="inputClick">{{ insertButtonText }}</button>
+
+    <button class="submit-button active:submit-button-active mt-2" @click="syncronizeClick">Syncronize FogOfWar</button>
+
+    <div class="flex flex-row mt-2">
+      <label class="switch">
+        <input type="checkbox" @change="gridSnapChanged" value="false">
+        <span class="slider round"></span>
+      </label>
+      <div class="flex items-center ml-2">Snap to Grid</div>
+    </div>
+
     <button class="button" @click="toggleTransparentMode">Toggle FogOfWar-SeeThrough</button>
-    <div class="hr"></div>
-    <button class="button" @click="syncronizeClick">Syncronize FogOfWar</button>
-    <div class="hr"></div>
-    <div><label>SnapGrid</label><input type="checkbox" @change="gridSnapChanged" value="false"></div>
+
   </div>
 </template>
-<style lang="scss">
-  @import "assets/css/scheme";
-</style>
 <script>
   import {
     DeletePolygon,
@@ -23,31 +34,98 @@
     toggleSnapToGrid, toggleTransparent
   } from "../../logic/stage/layers/fogofwar/main";
 
-  export default {
-    data() {
-      return {
-        InsertButtonText: "Insert",
-        insert: true,
-      }
+export default {
+  data() {
+    return {
+      insert: false,
+    }
+  },
+  methods: {
+    inputClick() {
+      !this.insert ? InsertPolygon() : DeletePolygon()
     },
-    methods: {
-      inputClick() {
-        this.insert ? InsertPolygon() : DeletePolygon()
-      },
-      toggleClick(){
-        toggleInsert();
-        this.insert = !this.insert;
-        this.InsertButtonText = !this.insert ? "Delete" : "Insert";
-      },
-      syncronizeClick(){
-        syncronize();
-      },
-      gridSnapChanged(){
-        toggleSnapToGrid();
-      },
-      toggleTransparentMode(){
-        toggleTransparent()
-      }
+    toggleClick() {
+      toggleInsert();
+    },
+    syncronizeClick() {
+      syncronize();
+    },
+    gridSnapChanged() {
+      toggleSnapToGrid();
+    },
+    toggleTransparentMode() {
+      toggleTransparent()
+    }
+  },
+  computed: {
+    insertButtonText() {
+      return (this.insert ? "Delete" : "Insert");
     }
   }
+}
 </script>
+<style lang="scss">
+@import "assets/css/scheme";
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 28px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #20880a;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 24px;
+  width: 24px;
+  left: 3px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #f32121;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #f32121;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(19px);
+  -ms-transform: translateX(19px);
+  transform: translateX(19px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
