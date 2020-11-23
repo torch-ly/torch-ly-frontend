ded90d06bba00b5b85e33bd59eb49ed208e1edb1
Caesar0815 <gingermk007@gmail.com>
2020-11-24 00:10:41 +0100

    started updated to ToggleBox.vue component

---
+++ components/gui-components/ToggleBox.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="flex flex-row mt-2">
+    <div class="flex items-center">
+      <label class="switch">
+        <input type="checkbox" v-model="value">
+        <span class="slider round"></span>
+      </label>
+    </div>
+
+    <div class="flex items-center ml-2 text-center">{{ title }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: this.checked
+    }
+  },
+  props: {
+    checked: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String
+    }
+  },
+  watch: {
+    value() {
+      this.$emit('update:checked', this.value);
+    },
+    checked() {
+      this.value = this.checked;
+    }
+  },
+  created() {
+    //this.$emit('update:value', this.value);
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 28px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #20880a;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 24px;
+  width: 24px;
+  left: 3px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #f32121;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #f32121;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(19px);
+  -ms-transform: translateX(19px);
+  transform: translateX(19px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
--- components/overlay/MeasureOverlay.vue
+++ components/overlay/MeasureOverlay.vue
@@ -15,27 +15,17 @@
       </div>
     </AdvancedOptions>
 
-    <div class="flex flex-row mt-2">
-      <div class="flex items-center">
-        <label class="switch">
-          <input type="checkbox" v-model="circularMeasure" @change="switchCircleMeasure">
-          <span class="slider round"></span>
-        </label>
-      </div>
+    <ToggleBox
+      title="Circle measure"
+      v-bind:checked.sync="measure.circular"
+      v-on:update:checked="$event ? setTool(measureTools.circle) : setTool(measureTools.line)"
+    />
 
-      <div class="flex items-center ml-2 text-center">Circle measure</div>
-    </div>
-
-    <div class="flex flex-row mt-2">
-      <div class="flex items-center">
-        <label class="switch">
-          <input type="checkbox" v-model="coneMeasure" @change="switchConeMeasure">
-          <span class="slider round"></span>
-        </label>
-      </div>
-
-      <div class="flex items-center ml-2 text-center">Cone measure</div>
-    </div>
+    <ToggleBox
+      title="Cone measure"
+      v-bind:checked.sync="measure.cone"
+      v-on:update:checked="$event ? setTool(measureTools.cone) : setTool(measureTools.line)"
+    />
 
     <button class="submit-button active:submit-button-active mt-2" v-show="savableMeasureToolActive"
             @click="saveMeasureAsPainting">Save as drawing
@@ -45,25 +35,27 @@
 </template>
 
 <script>
-import {store} from "../../logic/stage/main";
 import AdvancedOptions from "../gui-components/AdvancedOptions";
 import measureTools from "@/enums/tools/measureTools";
 import {mapActions} from "vuex";
 import tools from "@/enums/tools/tools";
 import {saveAsDrawing} from "~/logic/stage/layers/measure/circleMeasure";
+import ToggleBox from "@/components/gui-components/ToggleBox";
 
 export default {
+  components: {AdvancedOptions, ToggleBox},
   data() {
     return {
       tools,
       measureTools,
-      boxSize: store.state.manu.measureDetails.boxSize,
-      unitEnding: store.state.manu.measureDetails.unitEnding,
-      circularMeasure: false,
-      coneMeasure: false
+      boxSize: this.$store.state.manu.measureDetails.boxSize,
+      unitEnding: this.$store.state.manu.measureDetails.unitEnding,
+      measure: {
+        circular: false,
+        cone: false
+      }
     }
   },
-  components: {AdvancedOptions},
   computed: {
     getLength() {
       let measureDetails = this.$store.state.manu.measureDetails;
@@ -77,15 +69,18 @@ export default {
     }
   },
   watch: {
-    measureTool() {
-      this.coneMeasure = false;
-      this.circularMeasure = false;
+    measureTool(e) {
 
-      if (this.measureTool == this.measureTools.circle) {
-        this.circularMeasure = true;
-      } else if (this.measureTool == this.measureTools.cone) {
-        this.coneMeasure = true;
+      //TODO optimize this code
+
+      if (e === measureTools.circle) {
+        this.measure.circular = true;
+        this.measure.cone = false;
+      } else if (e === measureTools.cone) {
+        this.measure.circular = false;
+        this.measure.cone = true;
       }
+
     }
   },
   methods: {
@@ -93,7 +88,7 @@ export default {
       setTool: "manu/setTool"
     }),
     submitUnitChange() {
-      store.commit("manu/setMeasureDetails", {
+      this.$store.commit("manu/setMeasureDetails", {
         boxSize: this.boxSize,
         unitEnding: this.unitEnding
       })
@@ -101,24 +96,10 @@ export default {
     restoreDefault() {
       this.boxSize = 5;
       this.unitEnding = "ft";
-      store.commit("manu/setMeasureDetails", {
+      this.$store.commit("manu/setMeasureDetails", {
         boxSize: 5,
         unitEnding: "ft"
       })
-    },
-    switchCircleMeasure() {
-      if (this.measureTool != this.measureTools.line) {
-        this.setTool(this.measureTools.line);
-      } else {
-        this.setTool(this.measureTools.circle);
-      }
-    },
-    switchConeMeasure() {
-      if (this.measureTool != this.measureTools.line) {
-        this.setTool(this.measureTools.line);
-      } else {
-        this.setTool(this.measureTools.cone);
-      }
     },
     saveMeasureAsPainting() {
       if (this.$store.state.manu.measureTool === measureTools.circle) {
@@ -128,70 +109,6 @@ export default {
   }
 }
 </script>
-
 <style scoped lang="scss">
 @import "assets/css/scheme";
-
-/* The switch - the box around the slider */
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 50px;
-  height: 28px;
-}
-
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #f32121;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 24px;
-  width: 24px;
-  left: 3px;
-  bottom: 2px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #20880a;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #20880a;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(19px);
-  -ms-transform: translateX(19px);
-  transform: translateX(19px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
 </style>
