2b20d0fb1148127f62783e48b0d67f63c96788d6
Caesar0815 <gingermk007@gmail.com>
2020-11-28 16:16:22 +0100

    added parameter helper

--- logic/commandLine.js
+++ logic/commandLine.js
@@ -13,12 +13,31 @@ export function parseCommand(command) {
 function commandPattern(command, func, ...args) {
 	for (let arg of args)
 		if (command.startsWith(arg + " ") || command === arg)
-			func(command.replace(arg + " ", ""));
+			func(parameterHelper(command.replace(arg + " ", "")));
 
 }
 
+function parameterHelper(command) {
+	let parameters = {};
+	let lastCommand = "default";
 
-function roll(command) {
-	store.commit("dice/roll", command.replaceAll(" ", "+"));
+	for (let part of command.split(" "))
+		if (part.charAt(0) === "-")
+			if (part.charAt(1) === "-")
+				lastCommand = part.substring(2);
+			else
+				lastCommand = part.substring(1);
+
+		else if (!parameters[lastCommand])
+			parameters[lastCommand] = [ part ];
+		else
+			parameters[lastCommand].push(part);
+
+	return parameters;
+
+}
+
+function roll(parameters) {
+	store.commit("dice/roll", parameters.default.reduce((a, b) => a + "+" + b));
 }
 
