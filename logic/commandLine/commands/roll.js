5e2e83667f453801496e3901c7b99de2a9d5fb41
Caesar0815 <gingermk007@gmail.com>
2020-11-28 17:14:42 +0100

    added template for adv/dis rolls

--- logic/commandLine/commands/roll.js
+++ logic/commandLine/commands/roll.js
@@ -5,8 +5,20 @@ export function roll(parameters) {
 
 	bindParameterToFunction(parameters, _default);
 
+	bindParameterToFunction(parameters, advantage, "a", "advantage");
+
+	bindParameterToFunction(parameters, disadvantage, "d", "disadvantage");
+
 }
 
 function _default(parameters) {
 	store.commit("dice/roll", parameters.reduce((a, b) => a + "+" + b));
+}
+
+function advantage() {
+	console.warn("Advantage is currently not supported.");
+}
+
+function disadvantage() {
+	console.warn("Disadvantage is currently not supported.");
 }
